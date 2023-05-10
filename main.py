25a41e2684b26512fa9b49f43f18aaaac1d45a12
lherrman <lars.herrmann@ost.ch>
2023-05-10 14:10:26 +0200

    added worker window when worker is connected

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from camera_stream_server import CameraStreamServer
 from controll_stream_server import ControllStreamServer
 from pyslam_con import PySlamCon
 from base_model import CarModel
-from ui_elements import UIButton, UIText, ConfigWindow
+from ui_elements import UIButton, UIText, ConfigWindow, UIContainer
 from config import Config as cfg
 
 
@@ -34,23 +34,33 @@ class SlamcarController:
         self.ppu = 120 # pixels per unit 
         self.time_last_pressed = 0
 
-        self.connected = False
+        self.connected = False # Worker connected
         self.image_server = CameraStreamServer(port=cfg.get('image_stream_port'))
         self.controll_server = ControllStreamServer(port=cfg.get('controll_port'))
-
+        # Controlls that get sent to Worker
         self.controlls = {
             'throttle': 0.0,
             'steering': 0.0
         }
-
+        # UI elements
         self.ui_elements = []
         self._setup_ui()
-
+        # Configuration window
         self.config_window_width = 250
         self._config_window_x_pos = self.canvas_width
         self._config_window = ConfigWindow((self._config_window_x_pos,30), (self.config_window_width,self.canvas_height-30))
         self._show_config = False
 
+        self._show_camera_preview = False
+
+        # Connected Worker Window
+        self._connected_worker_window = None
+        # self._connected_worker_window = UIContainer((0, 400), (150, 200))
+        # self._connected_worker_window.add_element(UIText((0,0), "Worker 1", font_size=14))
+        #     #worker_ip = self.controll_server.get_worker_ip()
+        #     #self._connected_worker_window.add_element(UIText((0,20), f"{worker_ip}", font_size=14))
+        # self.ui_elements.append(self._connected_worker_window)
+        # PySlam Connection
         self.pyslam = PySlamCon(r'\\wsl.localhost\Ubuntu\home\user\slamcar\pyslam\videos\images')
 
     def run(self):
@@ -63,20 +73,24 @@ class SlamcarController:
             dt = self.clock.get_time() / 100
             self.screen.fill((30, 30, 30))
 
+            # Draw connected worker window
+            self._draw_connected_worker_window()
+
             # Update
             self.car.update(dt)
             self._update_controlls()
-
+            # Handle events
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.exit = True
                 if event.type == pg.USEREVENT:
+                    # Monitor config changes
                     if event.action == 'config_changed':
                         self.car.load_parameters()
                     if event.action == 'remote_config_changed':
                         self.controll_server.send_config(cfg.get('car_parameters'))
-                # react on scroll wheel
                 if event.type == pg.MOUSEBUTTONDOWN:
+                    # Zoom in and out with mouse wheel
                     if event.button == 4:
                         self.ppu += 10
                     elif event.button == 5:
@@ -88,16 +102,16 @@ class SlamcarController:
                 self._config_window.update(event)
             
             # Draw
-            self.car.draw(self.screen, self.ppu)            # draw car
-            self._draw_gui(self.screen)                     # draw gui
+            self.car.draw(self.screen, self.ppu)  # draw car
+            self._draw_gui(self.screen)           # draw gui
             for element in self.ui_elements:
                 element.draw(self.screen)
             
             # Draw configuration window with slide in animation
-            self._draw_configuration(self.screen)
+            self._draw_configuration()
 
             # Show image preview
-            self._show_image_preview()
+            self._reviece_images()
 
             self.clock.tick(self.ticks)
             pg.display.flip()
@@ -129,6 +143,8 @@ class SlamcarController:
         self.ui_elements.append(UIText((10, self.canvas_height-20),
                                     f"{local_ip}", font_size=14))
         
+    def _toggle_camera_preview(self):
+        self._show_camera_preview = not self._show_camera_preview
 
     def _toggle_configuration_window(self):
         self._show_config = not self._show_config
@@ -137,7 +153,7 @@ class SlamcarController:
         self._draw_hbar(screen, pos='top', height=30, color=(37, 37, 37))
 
         self._draw_connection_status_text(screen)
-        self._draw_configuration(screen)
+        self._draw_configuration()
 
     def _draw_hbar(self, screen, pos='top', height=30, color=(0, 0, 0)):
         '''Draw a horizontal bar at the top or bottom of the screen'''
@@ -164,9 +180,17 @@ class SlamcarController:
             text = self.font.render(f"waiting for connection " + 
                                "."*(self._gui_connection_text_helper+1), True, (255, 255, 255))
             screen.blit(text, position)
-            
 
-    def _draw_configuration(self, screen):
+    def _draw_connected_worker_window(self):    
+        if self._connected_worker_window is None and self.connected:   
+            self._connected_worker_window = UIContainer((20, 50), (150, 160))
+            self._connected_worker_window.add_element(UIText((40,20), "Worker 1", font_size=20))
+            self._connected_worker_window.add_element(UIButton((10,50), (130, 30), "Show Camera", self._toggle_camera_preview))
+            self._connected_worker_window.add_element(UIButton((10,80), (130, 30), "Open Image Folder", self._open_pyslam_image_folder))
+            self._connected_worker_window.add_element(UIButton((10,110), (130, 30), "Reinitialize", self._config_window.reinit_worker()))
+            self.ui_elements.append(self._connected_worker_window)
+
+    def _draw_configuration(self):
         if self._show_config: 
             config_window_target_x_pos = (self.canvas_width - self.config_window_width)
         else:
@@ -177,7 +201,10 @@ class SlamcarController:
         self._config_window.move_to(self._config_window_x_pos, 30)
         self._config_window.draw(self.screen)
 
-    def _show_image_preview(self):
+    def _open_pyslam_image_folder(self):
+        os.startfile(self.pyslam.image_dir)
+
+    def _reviece_images(self):
         '''Show camera preview in a separate window'''
         window_name = 'SlamCar Camera'
         if not hasattr(self, '_image_preview_last_image'):
@@ -189,18 +216,15 @@ class SlamcarController:
         image = self.image_server.receive_image()
         if image is not None:
             self.connected = True
-            cv2.imshow(window_name, image)
             self._image_preview_last_image = image
-
-            # SEND IMAGE TO SLAM
-            # TODO move this to a separate thread and other function
+      
             self.i_counter += 1
-            if self.i_counter % 20 == 0:
+            if self.i_counter % 2 == 0:
                 self._send_image_to_slam(image)
-
-
-        if self.connected:
+                
+        if self.connected and self._show_camera_preview:
             cv2.imshow(window_name, self._image_preview_last_image)
+
 
     def _get_local_ip(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
